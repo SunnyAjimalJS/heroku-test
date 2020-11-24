4ab7bb15d6f34888dcb84af348b0798ec44ddb2b
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-24 12:25:19 +0000

    First commit

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styles from "./LandingPage.module.scss";
+
+export default class LandingPage extends Component {
+  render() {
+    return <div className={styles.Container}>Landing page working</div>;
+  }
+}
---
+++ src/Container/App.js
@@ -0,0 +1,13 @@
+import LandingPage from "../Components/LandingPage";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      Heroku test App
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
